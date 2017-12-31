04b6da17c571c5aa28d5704144f6855fbf4bc5d7
Erick Delgado <erickdelgado@protonmail.com>
2017-12-31 15:31:04 -0500

    server added

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('build'));
+app.set('views', './build');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('/build/index.html'));
+});
+
+module.exports = app;
