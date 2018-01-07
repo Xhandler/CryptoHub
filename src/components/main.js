fa1b93895f11855dbffd61fd30b93353cd0227b5
Erick Delgado <erickdelgado@protonmail.com>
2018-01-07 15:47:44 -0500

    eslinting

--- src/components/main.js
+++ src/components/main.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import Component from 'react';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-
- class Main extends Component {
-   constructor(props) {
-     super(props);
-     this.state = {
-       message:'',
-     };
-   }
-   render() {
-     return(
-       <div>
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    };
+  }
+  render() {
+    return (
+      <div>
         <Grid fluid={true}>
           <Row>
-          <Col xs={12} sm={12} md={6} lg={6} >
-           <p>Hey there</p>
-           </Col>
-           <Col xs={12} sm={12} md={6} lg={6}>
-           <p>Hello</p>
-           </Col>
+            <Col xs={12} sm={12} md={6} lg={6} >
+              <p>Hey there</p>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <p>Hello</p>
+            </Col>
           </Row>
-        </Grid
-       </div>
-     );
-   }
- }
-
+        </Grid>
+      </div>
+    );
+  }
+}
 export default Main;
